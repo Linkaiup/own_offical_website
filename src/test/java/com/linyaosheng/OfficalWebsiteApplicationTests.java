8319be3c06736a89b0f49bb31e5342a6d1ac5bd9
Linkaiup <876487322@qq.com>
2017-11-22 14:25:19 +0800

    start a own_offical_website

---
+++ src/test/java/com/linyaosheng/OfficalWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linyaosheng;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OfficalWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
